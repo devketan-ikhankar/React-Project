83620612151e07a85dea808f22fbdfe5df64f942
ketan <ketanikhankar2@gmail.com>
2025-07-25 13:04:42 +0530

    commit

---
+++ dist/assets/Grocery-DWR_s-KX.js
@@ -0,0 +1,1 @@
+import{j as r,H as e}from"./index-BMETKl7E.js";const n=()=>r.jsxs("div",{children:[r.jsx(e,{}),r.jsx("h1",{children:" Grocery component along with differnt child component"})]});export{n as default};
