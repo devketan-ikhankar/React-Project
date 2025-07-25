a0b940281320cc785ed368c64c89707e2d2c1e3d
ketan <ketanikhankar2@gmail.com>
2025-07-25 13:07:21 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from "./Links/Contact.jsx";
 import LoginComponent from "./Links/LoginComponent.jsx";
 import Menu from "./Container/Menu.jsx";
 import Shimmer from "./Container/Shimmer.jsx";
-import { UserProvider } from "./Links/userContext.jsx";
+import { UserProvider } from "./Links/UserContext.jsx";
 import Signincomponent from "./Links/SigninComponent.jsx";
 import appStore from "./Links/AppStore.jsx";
 import Cart from "./Container/Cart.jsx";
